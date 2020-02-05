db6ef4ef0459f2efb9f7445d9d054a52ecb5cf90
Ian Vermes <ian.vermes@gmail.com>
2020-02-05 16:41:41 +0000

    Add explicity Brew install path for blueutil
    This is to support Platypus

--- restart.py
+++ restart.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- codeing: utf8 -*-
 """
 Restart OS X Bluetooth
@@ -7,21 +7,25 @@ OS X bluetooth devices sometimes disconnect and cannot reconnect without turn th
 systems Bluetooth support off and then back on.
 
 """
+import sys
 import subprocess
 import shlex
 import time
 
+PY3 = sys.version_info.major >= 3
+_BLUEUTIL = "/usr/local/bin/blueutil"
+
 
 def is_blueutil():
     # We need this program
-    command_s = "which blueutil"
+    command_s = "which %s" % _BLUEUTIL
     command = shlex.split(command_s)
     result = do_command(command, capture_output=True)
-    return result.returncode == 0
+    return result == 0
 
 
 def stop_bluetooth_service():
-    command_s = "blueutil -p 0"
+    command_s = "%s -p 0" % _BLUEUTIL
     command = shlex.split(command_s)
     do_command(command, check=True)
     print("BlueTooth OFF")
@@ -29,7 +33,7 @@ def stop_bluetooth_service():
 
 
 def start_bluetooth_service():
-    command_s = "blueutil -p 1"
+    command_s = "%s -p 1" % _BLUEUTIL
     command = shlex.split(command_s)
     do_command(command, check=True)
     print("BlueTooth ON")
@@ -41,9 +45,27 @@ def wait():
     time.sleep(1)
 
 
-def do_command(command, *_, **kwargs):
-    result = subprocess.run(command, **kwargs)
-    return result
+def do_command(command, check=False, capture_output=False):
+    if PY3:
+        process_obj = subprocess.run(
+            command, capture_output=capture_output, check=check
+        )
+        check_code = process_obj.returncode
+    else:
+        check_code = _do_command_py2(command, check, capture_output)
+    return check_code
+
+
+def _do_command_py2(command, check=False, capture_output=False):
+    if capture_output:
+        PIPE = subprocess.PIPE
+    else:
+        PIPE = None
+    if check:
+        check_code = subprocess.check_call(command, stdout=PIPE)
+    else:
+        check_code = subprocess.call(command, stdout=PIPE)
+    return check_code
 
 
 def main():
